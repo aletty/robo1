0725af951b0f83c4e6190071ef9d4065cf35ebaa
unknown <pvarin@4KKPWN1.olin.edu>
2013-12-09 19:04:38 -0500

    updating danger function

--- danger.m
+++ danger.m
@@ -1,6 +1,6 @@
 function danger
     % fake inputs for now
-    enemyPos = [200.5,200.5];
+    enemyPos = [300,300];
     
     % create axis variables
     x = linspace(1,1000);
@@ -13,6 +13,14 @@ function danger
     dist = R./sinc(T/pi);
     
     
-    d = exp(-dist.^2/100000) + exp(-R/20000) + tanh(exp(-((X-300+enemyPos(1)).^2 + (Y-300+enemyPos(2)).^2)/2000));
+    d = exp(-dist.^2/100000) + exp(-R/20000);
+    d = d + tanh(10*exp(-((X-500+enemyPos(1)).^2 + (Y-500+enemyPos(2)).^2)/800));
+    
+    Xp = X + enemyPos(1) - 680;
+    Yp = Y + enemyPos(2) - 516;
+    d = d + exp(-(sqrt(Xp.^2 + Yp.^2) - 600).^2/1000);
+%     d = d + 
+    
+    %calculate the danger for the pool edge
     surf(x,y,d)
 end
